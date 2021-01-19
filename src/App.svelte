a63d499e87fd087d7a7c6ad11a6ce78387e8f2f6
handrawanw <handrawanw@gmail.com>
2021-01-20 01:56:45 +0700

    update

--- src/App.svelte
+++ src/App.svelte
@@ -21,105 +21,104 @@
 		GetApiTicker();
 	});
 
-	let Data=[];
+	let options={};
 
 	store.subscribe((value)=>{
-		Data=value?.DataChart;
-	});
-
-	let options = {
-        chart: {
-    	type: 'candlestick',
-        height: 290,
-		id: 'candles',
-		background:"#000000",
-        toolbar: {
-        	autoSelected: 'pan',
-            show: false
-        },
-        zoom: {
-        	enabled: false
-        },
-    	},
-    	plotOptions: {
-          candlestick: {
-            colors: {
-              upward: '#3C90EB',
-              downward: '#DF7D46'
-            }
-          }
-        },
-        xaxis: {
-          type: 'datetime'
-		},
-		series: [{
-			data:Data
-		}],
-		dataLabels: {
-            enabled: false,
-        },
-        makers: {
-            size: 0,
-        },
-        colors: ["#f9ba42", "#84531d"],
-        stroke: {
-            width: [4, 4]
-        }, 
-        plotOptions: {
-            bar: {
-                columnWidth: "25%"
-            },
-        },
-        xaxis: {
-            type: 'category',
-            	axisBorder: {
-                show: false,
+		console.log(value.DataChart.length);
+		options={
+			chart: {
+			type: 'candlestick',
+			height: 500,
+			id: 'candles',
+			background:"#000000",
+			toolbar: {
+				autoSelected: 'pan',
+				show: false
 			},
-        labels: {
-            show:false,
-            formatter: function(val) {
-				return dayjs(val).format('MMM DD HH:mm')
-            }
-        },
-        grid: {
-                show: true,
-                borderColor: "white",
-                strokeDashArray: 0,
-                position: "back",
-                yaxis: {
-                    lines: {
-                        show: false,
-                    },
-                },
-            },
-            tickPlacement: 'on',
-        },
-        yaxis: {
-            labels: {
-                show: false
-            },
-        axisTicks: {
-            show: false
-        },
-        axisBorder: {
-            show: false,
-        },
-        },
-        tooltip: {
-            shared: true,
-            intersect: false,
-            x: {
-                show: false
-            },
-            y:{
-                show:false
-            },
-        },
-        stroke: {
-            width: 2
-        },
-	
-	};
+			zoom: {
+				enabled: false
+			},
+			},
+			plotOptions: {
+			candlestick: {
+				colors: {
+				upward: '#3C90EB',
+				downward: '#DF7D46'
+				}
+			}
+			},
+			xaxis: {
+			type: 'datetime'
+			},
+			series: [{
+				data:value?.DataChart
+			}],
+			dataLabels: {
+				enabled: false,
+			},
+			makers: {
+				size: 0,
+			},
+			colors: ["#f9ba42", "#84531d"],
+			stroke: {
+				width: [4, 4]
+			}, 
+			plotOptions: {
+				bar: {
+					columnWidth: "25%"
+				},
+			},
+			xaxis: {
+				type: 'category',
+					axisBorder: {
+					show: false,
+				},
+			labels: {
+				show:false,
+				formatter: function(val) {
+					return dayjs(val).format('MMM DD HH:mm')
+				}
+			},
+			grid: {
+					show: true,
+					borderColor: "white",
+					strokeDashArray: 0,
+					position: "back",
+					yaxis: {
+						lines: {
+							show: false,
+						},
+					},
+				},
+				tickPlacement: 'on',
+			},
+			yaxis: {
+				labels: {
+					show: false
+				},
+			axisTicks: {
+				show: false
+			},
+			axisBorder: {
+				show: false,
+			},
+			},
+			tooltip: {
+				shared: true,
+				intersect: false,
+				x: {
+					show: false
+				},
+				y:{
+					show:false
+				},
+			},
+			stroke: {
+				width: 2
+			},
+		
+		};
+	});
   </script>
 
 <style>
@@ -135,35 +134,35 @@
    
 <div class="container-fluid">
 	<div class="row">
-		<div class="col-8">
+		<div class="col-12">
 			<div class="d-flex justify-content-between">
 				<div class="div-konten">
-					CandleStick Price {Data.length}
+					CandleStick Price 
 				</div>
 			</div>
 			<div class="div-konten">
-				<button class="btn btn-success" on:click={()=>WebpacketData({resolutions:5,Data:Data})}>
+				<button class="btn btn-success" on:click={()=>WebpacketData({resolutions:5})}>
 					5 Minute
 				</button>
-				<button class="btn btn-success" on:click={()=>WebpacketData({resolutions:15,Data:Data})}>
+				<button class="btn btn-success" on:click={()=>WebpacketData({resolutions:15})}>
 					15 Minute
 				</button>
-				<button class="btn btn-success" on:click={()=>WebpacketData({resolutions:30,Data:Data})}>
+				<button class="btn btn-success" on:click={()=>WebpacketData({resolutions:30})}>
 					30 Minute
 				</button>
-				<button class="btn btn-success" on:click={()=>WebpacketData({resolutions:180,Data:Data})}>
+				<button class="btn btn-success" on:click={()=>WebpacketData({resolutions:180})}>
 					3 Hours
 				</button>
-				<button class="btn btn-success" on:click={()=>WebpacketData({resolutions:420,Data:Data})}>
+				<button class="btn btn-success" on:click={()=>WebpacketData({resolutions:420})}>
 					7 Hours
 				</button>
-				<button class="btn btn-success" on:click={()=>WebpacketData({resolutions:1440,Data:Data})}>
+				<button class="btn btn-success" on:click={()=>WebpacketData({resolutions:1440})}>
 					1 Days
 				</button>
-				<button class="btn btn-success" on:click={()=>WebpacketData({resolutions:1440,Data:Data})}>
+				<button class="btn btn-success" on:click={()=>WebpacketData({resolutions:1440*7})}>
 					1 Week
 				</button>
-				<button class="btn btn-success" on:click={()=>WebpacketData({resolutions:43200,Data:Data})}>
+				<button class="btn btn-success" on:click={()=>WebpacketData({resolutions:43200})}>
 					1 Month
 				</button>
 			</div>
